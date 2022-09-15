ffbac03cc61851bdfcca333eb70deac7b0a1f5e3
Ganesh Kosuri <ganeshkosuri9656@gmail.com>
2022-09-15 14:39:17 +0530

    add first basic vue project

---
+++ Vue Projects/vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <section>
+    <header>Hello Ganesh Kosuri</header>
+    <ul>
+      <friend-contact />
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: "1",
+          name: "Siva",
+          number: "78989478590",
+          email: "siva@gmail.com",
+        },
+        {
+          id: "2",
+          name: "Aravind",
+          number: "9133451679",
+          email: "aravind@gmail.com",
+        },
+        {
+          id: "3",
+          name: "Venkatesh",
+          number: "7845159632",
+          email: "venkatesh@gmail.com",
+        },
+      ],
+    };
+  },
+};
+</script>
