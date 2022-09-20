a3257f135c8610d88536ffe6b1d37ecd820833b9
Ganesh Kosuri <ganeshkosuri9656@gmail.com>
2022-09-20 13:02:49 +0530

    added small example by applying concepts

--- Vue Projects/cmp-adv-01-starting-setup/src/components/ActiveGoals.vue
+++ Vue Projects/cmp-adv-01-starting-setup/src/components/ActiveGoals.vue
@@ -2,9 +2,41 @@
   <div>
     <h2>Active Goals</h2>
     <label for="goal">Goal</label>
-    <input type="text" />
+    <input type="text" ref="goal" />
+    <button @click="setGoals">Set Goal</button>
+    <ErrorDialog v-if="isInputInvalid">
+      <h2>Input is Invalid</h2>
+      <p>Kindly enter a few charecters</p>
+      <button @click="updateErrorStatus">Okay</button>
+    </ErrorDialog>
   </div>
 </template>
+
+<script>
+import ErrorDialog from "./ErrorDialog.vue";
+
+export default {
+  components: {
+    ErrorDialog,
+  },
+  data() {
+    return {
+      isInputInvalid: false,
+    };
+  },
+  methods: {
+    setGoals() {
+      const enteredGoal = this.$refs.goal.value;
+      if (enteredGoal === "") {
+        this.isInputInvalid = true;
+      }
+    },
+    updateErrorStatus() {
+      this.isInputInvalid = false;
+    },
+  },
+};
+</script>
 
 <style scoped>
 label {
