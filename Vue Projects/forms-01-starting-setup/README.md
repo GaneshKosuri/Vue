40077603a4a4c95d9f1eff04bfca8f17dd6033fa
Ganesh Kosuri <ganeshkosuri9656@gmail.com>
2022-09-20 23:43:12 +0530

    add form inputs with v-models and custom cmp with v-model

---
+++ Vue Projects/forms-01-starting-setup/README.md
@@ -0,0 +1,11 @@
+# Section 11 Forms
+
+#### Things that I have learned in this chapter
+
+- v-model and Inputs
+- v-model modifiers and numbers
+- v-model with dropdowns
+- v-model with Checkbox and radio buttons
+- Form Basic Validation
+- Building Custom control components
+- v-model on custom components
---
+++ Vue Projects/forms-01-starting-setup/src/components/RatingControl.vue
@@ -0,0 +1,82 @@
+<template>
+  <ul>
+    <li>
+      <button
+        :class="{ active: modelValue === 'poor' }"
+        type="button"
+        @click="activate('poor')"
+      >
+        Poor
+      </button>
+    </li>
+    <li>
+      <button
+        :class="{ active: modelValue === 'average' }"
+        type="button"
+        @click="activate('average')"
+      >
+        Average
+      </button>
+    </li>
+    <li>
+      <button
+        :class="{ active: modelValue === 'great' }"
+        type="button"
+        @click="activate('great')"
+      >
+        Great
+      </button>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: ['modelValue'],
+  emits: ['update:modelValue'],
+  //   data() {
+  //     return {
+  //       activeOption: this.modelValue,
+  //     };
+  //   },
+  methods: {
+    activate(option) {
+      //   this.activeOption = option;
+      this.$emit('update:modelValue', option);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.active {
+  border-color: purple;
+}
+
+.active button {
+  color: chartreuse;
+}
+
+ul {
+  list-style: none;
+  margin: 1.5rem 0;
+  padding: 0;
+  display: flex;
+}
+
+li {
+  margin: 0 1rem;
+  border: 1px solid #ccc;
+  padding: 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+button {
+  font: inherit;
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+}
+</style>
